d867dd741dd75fc85f8ccede6ae82f2c2b71d02c
Daniel Carr <mail@danielcarr.co.za>
2022-12-28 02:31:16 +0200

    Show placeholder in input fields

---
+++ inputfield.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python3
+
+import tkinter.ttk as ttk
+
+
+class InputField(ttk.Entry):
+
+    def __init__(self, parent=None, placeholder=None, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+
+        self.placeholder = placeholder
+
+        self.bind('<FocusIn>', self.gained_focus)
+        self.bind('<FocusOut>', self.lost_focus)
+
+        self.set_empty(True)
+
+    def set_empty(self, isempty):
+        self.isempty = isempty
+        if isempty:
+            self.originalforeground = self['foreground']
+            self.originalshow = self['show']
+            self.configure(foreground='darkgrey', show='')
+            if self.instate(['focus']):
+                self.delete(0, 'end')
+            else:
+                self.insert(0, self.placeholder)
+        else:
+            self.delete(0, 'end')
+            self.configure(foreground=self.originalforeground)
+            self.configure(show=self.originalshow)
+
+    def gained_focus(self, *args):
+        if self.isempty:
+            self.set_empty(False)
+
+    def lost_focus(self, *args):
+        text = self.get()
+        if text == '' or text is None:
+            self.set_empty(True)
+        else:
+            self.set_empty(False)
+            self.insert(0, text)
+
+    def get(self):
+        if self.isempty:
+            return ''
+        else:
+            return super().get()
+
+    def set(self, value):
+        if value == '' or value is None:
+            self.set_empty(True)
+        elif self.isempty:
+            self.set_empty(False)
+            super().set(value)
--- widget.py
+++ widget.py
@@ -5,6 +5,8 @@ import hmac
 import tkinter as tk
 import tkinter.ttk as ttk
 
+from inputfield import InputField
+
 
 class HashapassWidget:
 
@@ -19,14 +21,14 @@ class HashapassWidget:
         self.window.rowconfigure(0, weight=1)
 
         self.parameter = tk.StringVar()
-        self.entry_parameter = ttk.Entry(frame, width=20, textvariable=self.parameter)
+        self.entry_parameter = InputField(frame, placeholder='Parameter', width=20, textvariable=self.parameter)
         self.entry_parameter.grid(column=0, row=0, sticky=(tk.W, tk.E))
         self.window.bind('<Return>', self.generate_password)
         self.window.bind('<Escape>', self.reset)
 
         self.password = tk.StringVar()
-        self.entry_master_password = ttk.Entry(frame, width=16, textvariable=self.password, show='*')
-        self.entry_master_password.grid(column=1, row=0, sticky=(tk.W, tk.E))
+        entry_master_password = InputField(frame, placeholder='Password', width=16, textvariable=self.password, show='*')
+        entry_master_password.grid(column=1, row=0, sticky=(tk.W, tk.E))
 
         self.character_count = tk.IntVar(value=10)
         spinbox_length = ttk.Spinbox(frame, width=2, from_=8, to=32, increment=1, state='readonly', textvariable=self.character_count)
