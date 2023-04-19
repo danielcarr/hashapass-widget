975f73ab574088aadb63e9294dba407dee397ce5
Daniel Carr <mail@danielcarr.co.za>
2023-04-19 13:13:17 +0200

    Organise setup code

--- widget.py
+++ widget.py
@@ -12,45 +12,56 @@ class HashapassWidget:
 
     def __init__(self, window):
 
+        # Setup container
         self.window = window
         self.window.resizable(width=False, height=False)
         self.window.title('Password Generator')
-        self.window.bind('<Return>', self.generate_password)
-        self.window.bind('<Escape>', self.reset)
-
-        frame = ttk.Frame(window, padding='1 1 1 1')
-        frame.grid(column=0, row=0, sticky=(tk.N, tk.E, tk.W, tk.S))
         self.window.columnconfigure(0, weight=1)
         self.window.rowconfigure(0, weight=1)
 
+        frame = ttk.Frame(window, padding='1 1 1 1')
+        frame.grid(column=0, row=0, sticky=(tk.N, tk.E, tk.W, tk.S))
+
+        # Field variables
+        self.character_count = tk.IntVar(value=10)
         self.parameter = tk.StringVar()
+        self.password = tk.StringVar()
+        self.result = tk.StringVar()
+
+        # Input fields
         self.entry_parameter = InputField(
                 frame, placeholder='Parameter',
                 width=20, textvariable=self.parameter)
-        self.entry_parameter.grid(column=0, row=0, sticky=(tk.W, tk.E))
 
-        self.password = tk.StringVar()
         self.entry_master_password = InputField(
                 frame, placeholder='Master Password',
                 width=16, show='*', textvariable=self.password)
-        self.entry_master_password.grid(column=1, row=0, sticky=(tk.W, tk.E))
 
-        self.character_count = tk.IntVar(value=10)
         spinbox_length = ttk.Spinbox(
                 frame, width=2, from_=8, to=32, increment=1,
                 state='readonly', textvariable=self.character_count)
-        spinbox_length.grid(column=2, row=0, sticky=(tk.W, tk.E))
 
-        self.result = tk.StringVar()
+        # Output display field
         entry_generated_password = ttk.Entry(
                 frame, width=16, font='TkFixedFont',
                 state='readonly', takefocus=0, textvariable=self.result)
-        entry_generated_password.grid(column=3, row=0, sticky=(tk.W, tk.E))
+
+        # Bind events
+        self.window.bind('<Return>', self.generate_password)
+        self.window.bind('<Escape>', self.reset)
         entry_generated_password.bind('<Double-Button-1>', self.copy_password)
+
+        # Layout widgets
+        horizontal = (tk.W, tk.E)
+        self.entry_parameter.grid(column=0, row=0, sticky=horizontal)
+        self.entry_master_password.grid(column=1, row=0, sticky=horizontal)
+        spinbox_length.grid(column=2, row=0, sticky=horizontal)
+        entry_generated_password.grid(column=3, row=0, sticky=horizontal)
 
         for child in self.window.winfo_children():
             child.grid_configure(padx=5, pady=2)
 
+        # Set initial UI state
         self.reset()
 
     def copy_password(self, event):
